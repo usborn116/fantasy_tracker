93fad507fb6ecc5411dd6e6e32173f0b40ee4777
Usborn Ocampo <usborn116@gmail.com>
2024-08-07 12:01:57 -0700

    let non-admins create trades and play with the trade machine

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -3,4 +3,10 @@ class Trade < ApplicationRecord
   belongs_to :team2, class_name: "Team", foreign_key: :team1_id
   belongs_to :team_season, optional: true
   belongs_to :league
+
+  after_create :set_status
+
+  def set_status
+    self.update(status: "pending")
+  end
 end
