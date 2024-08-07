9789952526aa57148ecd552b4b4057e109c432c4
Usborn Ocampo <usborn116@gmail.com>
2024-08-07 12:04:13 -0700

    let anyone see all NBA players, logged in or not

--- app/controllers/nba_pool/players_controller.rb
+++ app/controllers/nba_pool/players_controller.rb
@@ -1,6 +1,7 @@
 class NbaPool::PlayersController < ApplicationController
   before_action :set_nba_pool_player, only: %i[ show edit update destroy ]
   skip_before_action :set_league
+  skip_before_action :authenticate_user!, only: %i[ index show ]
 
   # GET /nba_pool/players or /nba_pool/players.json
   def index
