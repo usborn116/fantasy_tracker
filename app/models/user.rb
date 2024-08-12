3bb6cd4030e3b40bdbee6f7867f4d40bd00c29e9
Usborn Ocampo <usborn116@gmail.com>
2024-08-12 12:53:47 -0700

    changed some wording in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable
 
-  has_many :user_leagues
-  has_many :leagues
-  has_many :league_memberships, through: :user_leagues, class_name: "League"
+  has_many :league_memberships
+  has_many :owned_leagues, class_name: "League"
+  has_many :leagues, through: :league_memberships, class_name: "League"
   has_many :user_teams
   has_many :teams, through: :user_teams
 
-  accepts_nested_attributes_for :user_leagues
+  accepts_nested_attributes_for :league_memberships
   accepts_nested_attributes_for :user_teams
   accepts_nested_attributes_for :teams
 
