99a345e9a55cba0fed59e19c3ad3e5d920f600bc
Usborn Ocampo <usborn116@gmail.com>
2024-10-03 15:41:22 -0700

    added nba_api controller specs

--- spec/requests/nba_api_spec.rb
+++ spec/requests/nba_api_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "NbaApis", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+
+    it "runs the API fetch and returns the success response" do
+      @user = User.find_or_create_by(email: 'user_email@email.com')
+      @user.update!(name:'Usertest', encrypted_password:'userpassword', password: 'userpassword')
+      @user.confirm
+      sign_in @user
+      get '/api/nba_pool/nba_api'
+      expect(JSON.parse(response.body)['message']).to match("Successfully fetched NBA Player data")
+    end
+    
   end
+
 end
